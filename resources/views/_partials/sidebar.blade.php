d3e755370fcfc84336b6ab9c20f9b89bd2095951
hasininjato <hrambonimanana@gmail.com>
2022-12-30 23:51:49 +0300

    Première interface de l'application

---
+++ resources/views/_partials/sidebar.blade.php
@@ -0,0 +1,73 @@
+<aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#icons-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-house"></i><span>Classe</span>
+            </a>
+        </li><!-- End Classe Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-person"></i><span>Enseignants</span>
+            </a>
+        </li><!-- End Ensiegnants Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link " href="index.html">
+                <i class="bi bi-grid"></i>
+                <span>Etudiants</span>
+            </a>
+        </li><!-- End Etudiants Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#charts-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-book"></i><span>Matières</span>
+            </a>
+        </li><!-- End Matières Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-journal-text"></i><span>Niveaux</span>
+            </a>
+        </li><!-- End Niveaux Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#icons-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-pen"></i><span>Notes</span>
+            </a>
+        </li><!-- End Notes Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-layout-text-window-reverse"></i><span>Parcours</span>
+            </a>
+        </li><!-- End Parcours Nav -->
+
+        <li class="nav-heading">Pages</li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="users-profile.html">
+                <i class="bi bi-pie-chart"></i>
+                <span>Statistiques des étudiants</span>
+            </a>
+        </li><!-- End Profile Page Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="pages-faq.html">
+                <i class="bi bi-bar-chart"></i>
+                <span>Statistiques des notes</span>
+            </a>
+        </li><!-- End F.A.Q Page Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="pages-contact.html">
+                <i class="bi bi-envelope"></i>
+                <span>Générer un bulletin de notes</span>
+            </a>
+        </li><!-- End Contact Page Nav -->
+
+    </ul>
+
+</aside><!-- End Sidebar-->
