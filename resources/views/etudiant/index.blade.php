e8f2dc17058b85404d4527523caa956f09a7cf91
hasininjato <hrambonimanana@gmail.com>
2023-01-02 16:12:51 +0300

    Rechercher les étudiants par colonne

--- resources/views/etudiant/index.blade.php
+++ resources/views/etudiant/index.blade.php
@@ -10,7 +10,27 @@
     <link rel="stylesheet" href="{{ asset('vendor/datatables/css/dataTables.bootstrap.min.css') }}">
 
     <style>
+        table.dataTable tr.odd {
+            background-color: #e7e9ee;
+        }
 
+        table.dataTable tr.even {
+            background-color: white;
+        }
+
+        .dataTables_wrapper .dataTables_paginate .paginate_button:hover {
+            background: none;
+            background-color: #fff;
+            color: white !important;
+            border-color: white;
+            /*change the hover text color*/
+        }
+
+        tfoot input {
+            width: 100%;
+            padding: 3px;
+            box-sizing: border-box;
+        }
     </style>
 @endsection
 
@@ -39,13 +59,19 @@
                         <th>Nom</th>
                         <th>Prénom(s)</th>
                         <th>Sexe</th>
-                        <th>Date de naissance</th>
-                        <th>Lieu de naissance</th>
                         <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
                 </tbody>
+                <tfoot>
+                    <tr>
+                        <th>Matricule</th>
+                        <th>Nom</th>
+                        <th>Prénom(s)</th>
+                        <th>Sexe</th>
+                        </tr>
+                    </tfoot>
             </table>
         </div>
     </div>
@@ -57,41 +83,65 @@
 <script src="{{ asset('vendor/datatables/js/dataTables.bootstrap4.js') }}"></script>
 <script type="text/javascript">
     $(function() {
+        // Setup - add a text input to each footer cell
+        $('.yajra-datatable tfoot th').each(function() {
+            var title = $(this).text();
+            $(this).html('<input type="text" placeholder="' + title + '" />');
+        });
+
         var table = $('.yajra-datatable').DataTable({
             processing: true,
             serverSide: true,
             ajax: "{{ route('ajax-etudiants') }}",
+            language: {
+                "lengthMenu": "Afficher _MENU_ résultats par page",
+                "zeroRecords": "Aucun résultat",
+                "info": "Page _PAGE_ sur _PAGES_",
+                "infoEmpty": "Aucun enregistrement disponible",
+                "infoFiltered": "(filtered from _MAX_ total records)"
+            },
+            initComplete: function() {
+                // Apply the search
+                this.api()
+                    .columns()
+                    .every(function() {
+                        var that = this;
+
+                        $('input', this.footer()).on('keyup change clear', function() {
+                            if (that.search() !== this.value) {
+                                that.search(this.value).draw();
+                            }
+                        });
+                    });
+            },
             columns: [{
                     data: 'id_registration',
-                    name: 'id_registration'
+                    name: 'id_registration',
+                    "width": "3%",
                 },
                 {
                     data: 'lastname',
-                    name: 'lastname'
+                    name: 'lastname',
+                    "width": "35%",
                 },
                 {
                     data: 'firstname',
-                    name: 'firstname'
+                    name: 'firstname',
+                    "width": "35%",
                 },
                 {
                     data: 'sex',
-                    name: 'sex'
-                },
-                {
-                    data: 'birthday',
-                    name: 'birthday'
-                },
-                {
-                    data: 'place_birth',
-                    name: 'place_birth'
+                    name: 'sex',
+                    "width": "7%",
                 },
                 {
                     data: 'action',
                     name: 'action',
-                    orderable: true,
-                    searchable: true
+                    orderable: false,
+                    searchable: false,
+                    "width": "20%",
                 },
-            ]
+            ],
         });
     });
 
